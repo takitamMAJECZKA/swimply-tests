f672b85eeca2c37ab0fec347dbcde5e57ee44c11
Maciej Majka <magic_of_x@o2.pl>
2025-03-03 22:14:15 +0100

    trying with @ in workout import in home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 
 import { Link } from "react-router-dom"
-import AddNewWorkout from "../components/AddNewWorkout"
-import Workout from "../components/Workout"
+import AddNewWorkout from "@/components/AddNewWorkout"
+import Workout from "@/components/Workout"
 
 
 export default function Home(){

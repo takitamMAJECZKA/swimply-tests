feb3d8b7a279df488a095f6e76bf8d401764b695
Maciej Majka <magic_of_x@o2.pl>
2025-03-03 22:21:40 +0100

    trying with only button on stats page

--- src/Pages/Stats.jsx
+++ src/Pages/Stats.jsx
@@ -1,23 +1,7 @@
-import SideBar from '../components/SideBar'
-import { AppSidebar } from "@/components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
-
+import { Button } from "@/components/ui/button"'
 
 export default function Stats(){
     return (
-        <SideBar/>
+        <Button/>
       )
 }

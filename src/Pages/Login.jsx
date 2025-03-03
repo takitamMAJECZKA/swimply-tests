da727018069bf42f97c2dbf43c6f4ed9068f87fb
Maciej Majka <magic_of_x@o2.pl>
2025-03-03 22:46:20 +0100

    deleted login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,9 @@
-import LoginForm from "../components/LoginForm"
+// import LoginForm from "../components/LoginForm"
 
 
 export default function Login(){
     return(
     <div id="loginPage">
-    <LoginForm/>
+    {/* <LoginForm/> */}
     </div>)
 }
